51c4f03ab30862db28a0e858ac2e2e504b63bc9b
Buğra Keskin <bugrakeskin@gmail.com>
2024-11-12 15:07:28 +0300

    added welcome message

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,10 @@
 <template>
-  <div>
-    
+  <div class="text-4xl p-4 text-center font-bold">
+    Welcome to the Boiler Plate of Nuxt JS ver.3 compatible mode to 4 , Nuxt UI,
+    Tailwind CSS
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
